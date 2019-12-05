86349dae5f1e51dcbd3baa9f817a4f43e6b973bf
ylisovskyi_home_pc <lisovskyi.yurii@gmail.com>
2019-12-05 23:35:30 +0200

    Add code validator

--- app.py
+++ app.py
@@ -5,8 +5,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('main.html')
+def index_page():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+
+@app.route('/tasks')
+def tasks_page():
+    return render_template('tasks.html')
+
+
+@app.route('/code')
+def code_page():
+    return render_template('code.html')
 
 
 if __name__ == '__main__':
--- checker.py
+++ checker.py
@@ -1,17 +1,97 @@
+import ast
 import json
-import sys
-import traceback
+import os
+import subprocess
 
 
-def validate(source):
-    valid = True
-    try:
-        # ast.parse(source)
-        compile(source=source, filename='<unknown>', mode='exec', flags=1024)
-    except:
-        traceback.print_exc()
-        valid = False
-    return json.dumps({
-        'valid': valid,
-        'errors': [sys.exc_info()]
-    })
+def wrap_to_json(func):
+
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        if isinstance(res, tuple):
+            res = dict(res)
+
+        return json.dumps(res)
+
+    return wrapper
+
+
+class CodeValidator(object):
+
+    def __init__(self, compile_mode='exec'):
+        self.compile_mode = compile_mode
+
+    @wrap_to_json
+    def test_code(self, code, input_data, expected_output_data):
+        results = []
+        for input_sample, expected_output in zip(
+                input_data, expected_output_data
+        ):
+            if isinstance(input_sample, str):
+                input_sample = '"""{}"""'.format(input_sample)
+            tmp_code = code + '\n\nprint(main({}))'.format(input_sample)
+            validated = self.validate(tmp_code, type(expected_output))
+            if validated['valid']:
+                actual_output = validated['result']
+                validated['correct'] = (actual_output == expected_output)
+            results.append(validated)
+
+        return results
+
+    def compile_code(self, code, expected_type=None):
+        filename = 'file.py'
+        result = None
+        with open(filename, 'w') as f:
+            f.write(code)
+
+        sub = subprocess.Popen(
+            ['python', filename],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        res_output, res_error = sub.communicate()
+        res_output = res_output.decode('utf8')
+        res_error = res_error.decode('utf8')
+        if res_output:
+            if expected_type == str:
+                result = res_output.replace('\r', '').rstrip('\n')
+            else:
+                result = ast.literal_eval(res_output)
+        os.remove('file.py')
+        return result, res_error
+
+    def validate(self, source, expected_type=None):
+        out, err = self.compile_code(source, expected_type)
+        return {
+            'valid': False if err else True,
+            'correct': False,
+            'result': out,
+            'errors': [err]
+        }
+
+
+if __name__ == '__main__':
+    validator = CodeValidator()
+    code = """
+def main(l):
+    return l[::-1]
+"""
+    test_results = json.loads(
+        validator.test_code(
+            code=code,
+            input_data=[
+                [1, 2, 3],
+                'hello',
+                'hi\nhi',
+                [1, 2, 'hello']
+            ],
+            expected_output_data=[
+                [3, 2, 1],
+                'olleh',
+                'ih\nih',
+                ['hello', 3, 2]
+            ]
+        )
+    )
+    for res in test_results:
+        print(res)
